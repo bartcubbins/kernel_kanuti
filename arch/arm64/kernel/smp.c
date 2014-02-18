d47126cb226aade16e5a7ca2dd5413b796547b15
Abhimanyu Kapur <abhimany@codeaurora.org>
2014-02-18 09:36:37 -0800

    arm64: smp: move the pen to a header file
    Move the secondary_pen_release variable and the secondary_holding_pen
    entry function to asm/smp_plat.h so that the other cpu ops implementations
    can share them.
    
    Signed-off-by: Abhimanyu Kapur <abhimany@codeaurora.org>
    Signed-off-by: Kumar Gala <galak@codeaurora.org>
    Signed-off-by: Pavel Dubrova <pashadubrova@gmail.com>

--- arch/arm64/kernel/smp.c
+++ arch/arm64/kernel/smp.c
@@ -70,6 +70,7 @@ EXPORT_PER_CPU_SYMBOL(cpu_number);
 struct secondary_data secondary_data;
 /* Number of CPUs which aren't online, but looping in kernel text. */
 int cpus_stuck_in_kernel;
+volatile unsigned long secondary_holding_pen_release = INVALID_HWID;
 
 enum ipi_msg_type {
 	IPI_RESCHEDULE,
