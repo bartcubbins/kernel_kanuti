4fa8b9020358f7e17e11cee54e08f37e989bafc2
Pavel Dubrova <pashadubrova@gmail.com>
2018-03-13 13:50:16 +0000

    msm: clk: Add support for DSI PLL on MSM8939
    Add support for DSI PLL in clock driver for
    msm8939 target.
    
    Signed-off-by: Pavel Dubrova <pashadubrova@gmail.com>

--- drivers/clk/msm/mdss/mdss-dsi-pll-28lpm.c
+++ drivers/clk/msm/mdss/mdss-dsi-pll-28lpm.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2012-2016, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2012-2016, 2018, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -498,6 +498,7 @@ int dsi_pll_clock_register_lpm(struct platform_device *pdev,
 
 	if ((pll_res->target_id == MDSS_PLL_TARGET_8952) ||
 		(pll_res->target_id == MDSS_PLL_TARGET_8937) ||
+		(pll_res->target_id == MDSS_PLL_TARGET_8939) ||
 		(pll_res->target_id == MDSS_PLL_TARGET_8909)) {
 		if (!pll_res->index)
 			rc = of_msm_clock_register(pdev->dev.of_node,
