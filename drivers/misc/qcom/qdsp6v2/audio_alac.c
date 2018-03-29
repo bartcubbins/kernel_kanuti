0efcaa286c837857053b6067adb45185b72d1b5c
Alin Jerpelea <alin.jerpelea@sonymobile.com>
2018-03-29 18:52:48 -0500

    drivers: misc: qcom: qdsp6v2: fix DEBUG_FS ifdef
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sonymobile.com>

--- drivers/misc/qcom/qdsp6v2/audio_alac.c
+++ drivers/misc/qcom/qdsp6v2/audio_alac.c
@@ -314,11 +314,13 @@ static int audio_open(struct inode *inode, struct file *file)
 	}
 
 	snprintf(name, sizeof(name), "msm_alac_%04x", audio->ac->session);
+#ifdef CONFIG_DEBUG_FS
 	audio->dentry = debugfs_create_file(name, S_IFREG | S_IRUGO,
 					    NULL, (void *)audio,
 					    &audio_alac_debug_fops);
 
 	if (IS_ERR_OR_NULL(audio->dentry))
+#endif
 		pr_debug("debugfs_create_file failed\n");
 	pr_debug("%s:alacdec success mode[%d]session[%d]\n", __func__,
 						audio->feedback,
