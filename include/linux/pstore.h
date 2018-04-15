376139e0945e4bf81afb3c31ead1fe12fb334a3e
Pavel Dubrova <pashadubrova@gmail.com>
2018-04-15 11:12:39 +0000

    pstore from 3.18
    Signed-off-by: Pavel Dubrova <pashadubrova@gmail.com>

--- include/linux/pstore.h
+++ include/linux/pstore.h
@@ -41,7 +41,6 @@ enum pstore_type_id {
 	PSTORE_TYPE_PPC_OF	= 5,
 	PSTORE_TYPE_PPC_COMMON	= 6,
 	PSTORE_TYPE_PMSG	= 7,
-	PSTORE_TYPE_PPC_OPAL	= 8,
 	PSTORE_TYPE_UNKNOWN	= 255
 };
 
@@ -59,8 +58,7 @@ struct pstore_info {
 	int		(*close)(struct pstore_info *psi);
 	ssize_t		(*read)(u64 *id, enum pstore_type_id *type,
 			int *count, struct timespec *time, char **buf,
-			bool *compressed, ssize_t *ecc_notice_size,
-			struct pstore_info *psi);
+			bool *compressed, struct pstore_info *psi);
 	int		(*write)(enum pstore_type_id type,
 			enum kmsg_dump_reason reason, u64 *id,
 			unsigned int part, int count, bool compressed,
@@ -79,14 +77,22 @@ struct pstore_info {
 	void		*data;
 };
 
-#define PSTORE_FLAGS_DMESG	(1 << 0)
-#define PSTORE_FLAGS_FRAGILE	PSTORE_FLAGS_DMESG
-#define PSTORE_FLAGS_CONSOLE	(1 << 1)
-#define PSTORE_FLAGS_FTRACE	(1 << 2)
-#define PSTORE_FLAGS_PMSG	(1 << 3)
+#define	PSTORE_FLAGS_FRAGILE	1
 
+#ifdef CONFIG_PSTORE
 extern int pstore_register(struct pstore_info *);
-extern void pstore_unregister(struct pstore_info *);
 extern bool pstore_cannot_block_path(enum kmsg_dump_reason reason);
+#else
+static inline int
+pstore_register(struct pstore_info *psi)
+{
+	return -ENODEV;
+}
+static inline bool
+pstore_cannot_block_path(enum kmsg_dump_reason reason)
+{
+	return false;
+}
+#endif
 
 #endif /*_LINUX_PSTORE_H*/
