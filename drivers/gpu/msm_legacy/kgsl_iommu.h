2d328ac64d13e6753f54d3932f75dcbc19750602
Pavel Dubrova <pashadubrova@gmail.com>
2018-04-24 17:17:46 +0000

    msm: kgsl_legacy: Update various exernal APIs for the 4.4 kernel
    Make several changes to build the GPU driver for 4.4:
    
     - Rename CONFIG_MSM to CONFIG_QCOM where applicable
     - Add msm_kgsl.h to the Kbuild exports
     - Remove linux/coresight_of.h (as it has been merged into
       coresight.h) and remove the .owner member of the
       coresight_desc struct.
     - Use the new location for the sync.h file (in staging)
     - Remove an unused sync function
     - Move oneshot_sync.h inside of #ifdef wrappers
    
    Signed-off-by: Jordan Crouse <jcrouse@codeauorora.org>
    Signed-off-by: Pavel Dubrova <pashadubrova@gmail.com>

--- drivers/gpu/msm_legacy/kgsl_iommu.h
+++ drivers/gpu/msm_legacy/kgsl_iommu.h
@@ -1,4 +1,4 @@
-/* Copyright (c) 2012-2015, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2012-2015, 2018, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -13,7 +13,7 @@
 #ifndef __KGSL_IOMMU_H
 #define __KGSL_IOMMU_H
 
-#ifdef CONFIG_MSM_IOMMU
+#ifdef CONFIG_QCOM_IOMMU
 #include <linux/qcom_iommu.h>
 #endif
 #include <linux/of.h>
