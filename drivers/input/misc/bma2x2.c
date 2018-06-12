19a829f5369511fcdb49ff87497f536e0edfbab9
Daniel Vasquez <danielgusvt@yahoo.com>
2018-06-11 23:41:50 -0500

    input: misc: bma2x2: Don't enable the id header support for kanuti
    If we don't disable this, we will break the driver
    functionality.
    
    Signed-off-by: Daniel Vasquez <danielgusvt@yahoo.com>

--- drivers/input/misc/bma2x2.c
+++ drivers/input/misc/bma2x2.c
@@ -67,7 +67,9 @@
 #define ISR_INFO(dev, fmt, arg...)
 #endif
 
+#ifndef CONFIG_ARCH_SONY_KANUTI
 #define BMA2X2_SENSOR_IDENTIFICATION_ENABLE
+#endif
 
 #define SENSOR_NAME                 "bma2x2-accel"
 #define ABSMIN                      -512
