sonyxperiadev/kernel

input: touchscreen: cyttsp4/5: Do not probe cyttsp5 if cyttsp4 detected
Tulip  device has only one version of the touchscreen. After detection
the correct one, it makes no sense to probe another one.

Signed-off-by: Pavel Dubrova <pashadubrova@gmail.com>

drivers/input/input.c
@@ -40,6 +40,11 @@ static DEFINE_IDA(input_ida);
 static LIST_HEAD(input_dev_list);
 static LIST_HEAD(input_handler_list);
 
+#ifdef CONFIG_MACH_SONY_TULIP
+/* cyttsp detection */
+bool cyttsp_i2c_driver = false;
+#endif
+
 /*
  * input_mutex protects access to both input_dev_list and input_handler_list.
  * This also causes input_[un]register_device and input_[un]register_handler

drivers/input/touchscreen/cyttsp4/cyttsp4_i2c.c
@@ -51,6 +51,11 @@
 
 #define CY_I2C_DATA_SIZE  (3 * 256)
 
+#ifdef CONFIG_MACH_SONY_TULIP
+/* cyttsp detection */
+extern bool cyttsp_i2c_driver;
+#endif
+
 struct cyttsp4_i2c {
 	struct i2c_client *client;
 	u8 wr_buf[CY_I2C_DATA_SIZE];
@@ -312,6 +317,11 @@ static int cyttsp4_i2c_probe(struct i2c_client *client,
 	}
 
 	dev_info(dev, "%s: Successful probe %s\n", __func__, CYTTSP4_I2C_NAME);
+
+#ifdef CONFIG_MACH_SONY_TULIP
+	/* cyttsp detection */
+	cyttsp_i2c_driver = true;
+#endif
 
 	return 0;
 

drivers/input/touchscreen/cyttsp5/cyttsp5_i2c.c
@@ -29,6 +29,11 @@
 
 #define CY_I2C_DATA_SIZE  (2 * 256)
 
+#ifdef CONFIG_MACH_SONY_TULIP
+/* cyttsp detection */
+extern bool cyttsp_i2c_driver;
+#endif
+
 static int cyttsp5_i2c_read_default(struct device *dev, void *buf, int size)
 {
 	struct i2c_client *client = to_i2c_client(dev);
@@ -147,6 +152,12 @@ static int cyttsp5_i2c_probe(struct i2c_client *client,
 	struct regulator *vreg_l27;
 	int rc;
 	int retval;
+
+#ifdef CONFIG_MACH_SONY_TULIP
+	/* cyttsp detection */
+	if (cyttsp_i2c_driver)
+		return 0;
+#endif
 
 	dev_info(dev, "%s: Starting %s probe...\n", __func__, CYTTSP5_I2C_NAME);
 

bartcubbins/kernel_kanuti

drivers/input/input.c
@@ -40,6 +40,11 @@ static DEFINE_IDA(input_ida);
 static LIST_HEAD(input_dev_list);
 static LIST_HEAD(input_handler_list);
 
+#ifdef CONFIG_ARCH_SONY_KANUTI
+/* cyttsp detection */
+bool cyttsp_i2c_driver = false;
+#endif
+
 /*
  * input_mutex protects access to both input_dev_list and input_handler_list.
  * This also causes input_[un]register_device and input_[un]register_handler

drivers/input/touchscreen/cyttsp4/cyttsp4_i2c.c
@@ -51,6 +51,11 @@
 
 #define CY_I2C_DATA_SIZE  (3 * 256)
 
+#ifdef CONFIG_ARCH_SONY_KANUTI
+/* cyttsp detection */
+extern bool cyttsp_i2c_driver;
+#endif
+
 struct cyttsp4_i2c {
 	struct i2c_client *client;
 	u8 wr_buf[CY_I2C_DATA_SIZE];
@@ -312,6 +317,11 @@ static int cyttsp4_i2c_probe(struct i2c_client *client,
 	}
 
 	dev_info(dev, "%s: Successful probe %s\n", __func__, CYTTSP4_I2C_NAME);
+
+#ifdef CONFIG_ARCH_SONY_KANUTI
+	/* cyttsp detection */
+	cyttsp_i2c_driver = true;
+#endif
 
 	return 0;
 

drivers/input/touchscreen/cyttsp5/cyttsp5_i2c.c
@@ -29,6 +29,11 @@
 
 #define CY_I2C_DATA_SIZE  (2 * 256)
 
+#ifdef CONFIG_ARCH_SONY_KANUTI
+/* cyttsp detection */
+extern bool cyttsp_i2c_driver;
+#endif
+
 static int cyttsp5_i2c_read_default(struct device *dev, void *buf, int size)
 {
 	struct i2c_client *client = to_i2c_client(dev);
@@ -147,6 +152,12 @@ static int cyttsp5_i2c_probe(struct i2c_client *client,
 	struct regulator *vreg_l27;
 	int rc;
 	int retval;
+
+#ifdef CONFIG_ARCH_SONY_KANUTI
+	/* cyttsp detection */
+	if (cyttsp_i2c_driver)
+		return 0;
+#endif
 
 	dev_info(dev, "%s: Starting %s probe...\n", __func__, CYTTSP5_I2C_NAME);
 